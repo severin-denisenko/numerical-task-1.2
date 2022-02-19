fa307cf4465b9282abdfbd7f87e248c0510e54b3
severin-denisenko <devchonkavkorobchonke@gmail.com>
2022-02-19 14:47:56 +0300

    abracadabra1

--- matrix.h
+++ matrix.h
@@ -5,7 +5,7 @@
 #ifndef INC_1_1_MATRIX_H
 #define INC_1_1_MATRIX_H
 
-int stupid_multiply(double *A, double *B, double *C, int n);
+int diagonal_3_multiply(double *A, double *B, double *C, int n);
 int optimised_stupid_multiply(double *A, double *B, double *C, int n);
 
 #endif //INC_1_1_MATRIX_H
